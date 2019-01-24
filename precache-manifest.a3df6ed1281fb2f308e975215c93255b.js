ae3d64cf3077b11daed4d73877c1b563335383a5
ponlia <lia.pon@mail.utoronto.ca>
2019-01-23 16:43:10 -0800

    Updates

---
+++ precache-manifest.a3df6ed1281fb2f308e975215c93255b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "eec1172b6892b45f1f92",
+    "url": "/website/static/js/main.eec1172b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "eec1172b6892b45f1f92",
+    "url": "/website/static/css/main.96ef5bb3.chunk.css"
+  },
+  {
+    "revision": "d86aae0d43a1c387a7af2b9d6848f3fa",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77cba747353b991ab42e5a750d4ce575.js"
+  "/website/precache-manifest.a3df6ed1281fb2f308e975215c93255b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
